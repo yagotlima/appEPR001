32bc60fd8d9315ce7c69f982a19d54df173312af
Yago Lima <yagotoledolima@gmail.com>
2014-11-25 16:46:47 -0200

    platforms e plugins adicionados ao repositorio

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/yago/Programas/Mobile/Phonegap/app-EPR001/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/yago/Programas/Mobile/Phonegap/app-EPR001/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/yago/Programas/Mobile/Phonegap/app-EPR001/platforms/android/ant-build/AndroidManifest.xml : \
+/home/yago/Programas/Mobile/Phonegap/app-EPR001/platforms/android/AndroidManifest.xml \
