3c3555d2487825f1915ca3e1287a5fa1f8f4fe02
Yago Lima <yagotoledolima@gmail.com>
2014-11-26 00:43:19 -0200

    Implementos calculo de taxa de juros e rendimento

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -59,6 +59,59 @@ function abreView(view, titulo) {
 }
 
 function calculaTaxaJuros() {
-	
+    var pv = $('#form1 input[name=vista]').val() - $('#form1 input[name=entrada]').val();
+    var pmt = $('#form1 input[name=prazo]').val();
+    var n = $('#form1 input[name=vezes]').val();
+    
+    var min = 0;
+    var max = 200;
+    var taxa, erroAnterior, erro = 99999999999999.0;
+    var valorPresente, valorFuturo;
+    const PRECISAO = 0.0000001;
+
+    while(true){
+        taxa = (min + max) / 2.0;
+        erroAnterior = erro;
+        valorFuturo = pmt * ((Math.pow((taxa+1), n)-1)/taxa);
+        valorPresente = valorFuturo/Math.pow(taxa+1.0, n);
+        erro = pv - valorPresente;
+
+        if(Math.abs(erro) < PRECISAO || Math.abs(erro - erroAnterior) < PRECISAO){
+            $('#taxa').text((taxa*100).toFixed(3) + "%");
+            break;
+        }
+        
+        if(erro > 0)
+			max = taxa;
+		else
+			min = taxa;
+    }
+
+	taxa = Math.pow(taxa + 1, 12) -1 //Taxa ao ano
+
+	$('#form1 #taxa').text(taxa);
+	$('#form1 #resultado').text(taxa > parseFloat($('#form1 #poupanca').html()) ? 'Compre a vista!' : 'Compre a prazo!' );
+	$('#form1 #resultado').css('color', '#f00');
+}
+
+function calculaRendimentos() {
+    var taxa = $('#form2 input[name=taxa]').val() / 100;
+    var capitalInicial = $('#form2 input[name=inicial]').val();
+    var periodo = $('#form2 input[name=vezes]').val();
+    var aplicacoes = $('#form2 input[name=prazo]').val();
+    var rendimento, total;
+
+    var montante1, montante2;
+
+    montante1 = capitalInicial * Math.pow((1+taxa), periodo);
+    montante2 = aplicacoes*(1+taxa) * ((Math.pow((1+taxa), periodo) - 1) / taxa);
+
+    rendimento = (montante2 + montante1) - capitalInicial - periodo*aplicacoes;
+    total = montante1 + montante2;
+
+	$('#form2 #resultado').text('Valor ao final da aplicação: ' + total);
+}
+
+function atualizaTaxas() {
 }
 
